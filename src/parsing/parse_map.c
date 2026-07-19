f564bbf9101f7d2c3a6d5585fd9b3837ce356d80
awadanthony33-collab <awadanthony33@gmail.com>
2026-07-19 17:51:14 +0300

    repear validate

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -4,18 +4,21 @@ void parse_grid_line(char *line , t_game *game)
 {
 
     char **grid;
+    char *tmp;
     int i;
     grid = malloc((game->map.height + 2) * sizeof(char *));
+    if(!grid)
+        return;
     i=0;
     while(i<game->map.height)
     {
         grid[i] = game->map.grid[i];
         i++;
     }
-    grid[i] = ft_strdup(line);
+    tmp = ft_strtrim(line, "\n");
+    grid[i] = tmp;
     grid[i+1] = NULL;
- 
+    free(game->map.grid);
     game->map.grid = grid;
-
     game->map.height++;
 } 
--- src/parsing/validate_maps.c
+++ src/parsing/validate_maps.c
@@ -3,7 +3,7 @@
 static int maps_valid_character(char c)
 {
     if(c == '0' || c == '1' || c=='N' || c=='S' || c == 'E' || c == 'W' || c == ' ')
-        return 1;
+        return (1);
     return 0;
 }
 
@@ -18,11 +18,21 @@ int validate_map(t_game *game)
     while (game->map.grid[y])
     {
         x=0;
-        while(game->map.grid[x][y])
+        while(game->map.grid[y][x])
         {
-            while(!maps_valid_character(game->map.grid[y][x]))
-                return 1;
-            if(game->map.grid[x][y] == 'N' || game->map.grid[x][y] == 'S' ||  game->map.grid[x][y] == 'E' || game->map.grid[x][y] == 'W'  )
+        if (!maps_valid_character(game->map.grid[y][x]))
+        {
+            printf("Invalid character '%c' (ASCII %d) at [%d][%d]\n",
+                game->map.grid[y][x],
+                game->map.grid[y][x],
+                y,
+                x);
+            return (error_exit(game, "Invalid map character"));
+        }
+            if (game->map.grid[y][x] == 'N' ||
+                game->map.grid[y][x] == 'S' ||
+                game->map.grid[y][x] == 'E' ||
+                game->map.grid[y][x] == 'W')
                 player_count++;
             x++;
         }
