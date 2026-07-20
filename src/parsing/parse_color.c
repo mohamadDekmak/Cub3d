64b7fbbd1724b021c77e5bfd8447d88fdfab6b48
awadanthony33-collab <awadanthony33@gmail.com>
2026-07-20 20:47:59 +0300

    FREE SPLIT

--- src/parsing/parse_color.c
+++ src/parsing/parse_color.c
@@ -1,5 +1,18 @@
 #include "cub3d.h"
 
+void free_split(char **split)
+{
+    int i;
+
+    i = 0;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+}
+
 void parse_color(char *line , t_game *game)
 {
 	char	**rgb;
@@ -11,7 +24,7 @@ void parse_color(char *line , t_game *game)
 	r = ft_atoi(rgb[0]);
 	g = ft_atoi(rgb[1]);
 	b = ft_atoi(rgb[2]);
-
+	free_split(rgb);
 	if(line[0] == 'F')
 	{
 		game->map.fl_color = get_rgb(r, g, b);
